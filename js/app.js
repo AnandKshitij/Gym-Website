bfd70a2bd2e9888f1904003d6c30dd24de71411d
kshitijanand <kshitijanand0512@gmail.com>
2021-05-07 02:02:19 +0530

    express installed

---
+++ js/app.js
@@ -0,0 +1,11 @@
+// IMPORTING EXPRESS
+const express = require("express");
+const app = express();
+
+// IMPORTING FILE SYSTEM
+const fs = require("fs");
+
+// PORT THAT WE WILL BE USING
+const port = 80;
+
+console.log("Chal jaa yaar");
